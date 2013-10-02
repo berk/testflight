9cab9e53d1e12e194dc5c0e50cd9e57a40911178
Dominik Grygiel <dominik.grygiel@gmail.com>
2013-10-02 13:23:01 +0200

    this should be enough

--- lib/testflight/builder.rb
+++ lib/testflight/builder.rb
@@ -79,7 +79,7 @@ module Testflight
       cmd << "-scheme '#{Testflight::Config.application_name}' "
       cmd << "-sdk '#{Testflight::Config.sdk_version}' "
       cmd << "-configuration 'AdHoc' "
-      cmd << "-arch 'armv7 armv7s' "
+      cmd << "-arch 'armv7' "
       cmd << "CONFIGURATION_BUILD_DIR='#{Testflight::Config.build_dir}' "
       execute(cmd, opts)
     end
